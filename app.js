532ca6385c6611b1031c869419e025e759463b46
thpss91103 <a0975142932@gmail.com>
2023-05-09 20:53:44 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 const app = express()
 const PORT = 3000
 
+const passport = require('passport')
+
+const usePassport = require('./config/passport')
+
 const db = require('./models')
 const Todo = db.Todo
 const User = db.User
+
+usePassport(app)
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.get('/', (req, res) => {
   return Todo.findAll({
@@ -36,9 +48,10 @@ app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 app.get('/users/register', (req, res) => {
   res.render('register')
