cc7a09c033aae9b94ca948bd557ebd8522632e51
thpss91103 <a0975142932@gmail.com>
2023-05-09 22:40:56 +0800

    feat: add new function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -9,9 +9,9 @@ router.get('/new', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-  const userId = req.user._id
+  const UserId = req.user.id
   const name = req.body.name
-  return Todo.create({ name, userId })
+  return Todo.create({ name, UserId })
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
@@ -24,18 +24,6 @@ router.get('/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
-router.put('/:id', (req, res) => {
-  const UserId = req.user.id
-  const _id = req.params.id
-  const { name, isDone } = req.body
-  return Todo.findOne({ _id, userId })
-    .then(todo => {
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      return todo.save()
-    })
-    .then(() => res.redirect(`/todos/${_id}`))
-    .catch(error => console.log(error))
-})
+
 
 module.exports = router
